daa451ced275f0f6c92039106a1ff123e2886c4b
Alex Thomson <LXGaming@users.noreply.github.com>
2019-05-07 16:56:20 +1200

    Update dependencies

--- velocity/src/main/java/nz/co/lolnet/location/velocity/util/VelocityToolbox.java
+++ velocity/src/main/java/nz/co/lolnet/location/velocity/util/VelocityToolbox.java
@@ -19,7 +19,6 @@ package nz.co.lolnet.location.velocity.util;
 import com.velocitypowered.proxy.connection.MinecraftConnection;
 import com.velocitypowered.proxy.network.Connections;
 import io.netty.channel.Channel;
-import net.kyori.text.Components;
 import net.kyori.text.TextComponent;
 import net.kyori.text.event.ClickEvent;
 import net.kyori.text.event.HoverEvent;
@@ -35,24 +34,24 @@ public class VelocityToolbox {
     
     public static TextComponent getTextPrefix() {
         TextComponent.Builder textBuilder = TextComponent.builder();
-        textBuilder.hoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, getPluginInformation()));
+        textBuilder.hoverEvent(HoverEvent.of(HoverEvent.Action.SHOW_TEXT, getPluginInformation()));
         textBuilder.content("[" + Reference.NAME + "]").color(TextColor.BLUE).decoration(TextDecoration.BOLD, true);
         return TextComponent.of("").append(textBuilder.build()).append(TextComponent.of(" "));
     }
     
     public static TextComponent getPluginInformation() {
         TextComponent.Builder textBuilder = TextComponent.builder("");
-        textBuilder.append(TextComponent.of(Reference.NAME, TextColor.BLUE).decoration(TextDecoration.BOLD, true)).append(Components.newline());
-        textBuilder.append(TextComponent.of("    Version: ", TextColor.DARK_GRAY)).append(TextComponent.of(Reference.VERSION, TextColor.WHITE)).append(Components.newline());
-        textBuilder.append(TextComponent.of("    Authors: ", TextColor.DARK_GRAY)).append(TextComponent.of(Reference.AUTHORS, TextColor.WHITE)).append(Components.newline());
-        textBuilder.append(TextComponent.of("    Source: ", TextColor.DARK_GRAY)).append(getURLTextAction(Reference.SOURCE)).append(Components.newline());
+        textBuilder.append(TextComponent.of(Reference.NAME, TextColor.BLUE).decoration(TextDecoration.BOLD, true)).append(TextComponent.newline());
+        textBuilder.append(TextComponent.of("    Version: ", TextColor.DARK_GRAY)).append(TextComponent.of(Reference.VERSION, TextColor.WHITE)).append(TextComponent.newline());
+        textBuilder.append(TextComponent.of("    Authors: ", TextColor.DARK_GRAY)).append(TextComponent.of(Reference.AUTHORS, TextColor.WHITE)).append(TextComponent.newline());
+        textBuilder.append(TextComponent.of("    Source: ", TextColor.DARK_GRAY)).append(getURLTextAction(Reference.SOURCE)).append(TextComponent.newline());
         textBuilder.append(TextComponent.of("    Website: ", TextColor.DARK_GRAY)).append(getURLTextAction(Reference.WEBSITE));
         return textBuilder.build();
     }
     
     public static TextComponent getURLTextAction(String url) {
         TextComponent.Builder textBuilder = TextComponent.builder();
-        textBuilder.clickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, url));
+        textBuilder.clickEvent(ClickEvent.of(ClickEvent.Action.OPEN_URL, url));
         textBuilder.content(url).color(TextColor.BLUE);
         return textBuilder.build();
     }
